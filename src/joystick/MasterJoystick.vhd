b65dfb17cedbfaffdb5395eab205ed4dd999a2f7
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-11-19 18:01:26 +0100

    feat(joystick): working joystick version

--- src/joystick/MasterJoystick.vhd
+++ src/joystick/MasterJoystick.vhd
@@ -6,21 +6,34 @@ ENTITY MasterJoystick IS
 		rst     : IN STD_LOGIC;
 		clk     : IN STD_LOGIC;
 		en      : IN STD_LOGIC;
-		v1      : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		v2      : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+		inLed1  : IN STD_LOGIC;
+		inLed2  : IN STD_LOGIC;
 		miso    : IN STD_LOGIC;
 		ss      : OUT STD_LOGIC;
 		sclk    : OUT STD_LOGIC;
 		mosi    : OUT STD_LOGIC;
-		val_xor : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-		val_and : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-		val_or  : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+		x_joy   : OUT STD_LOGIC_VECTOR (9 DOWNTO 0);
+		y_joy   : OUT STD_LOGIC_VECTOR (9 DOWNTO 0);
+		btn_joy : OUT STD_LOGIC;
+		btn_1   : OUT STD_LOGIC;
+		btn_2   : OUT STD_LOGIC;
 		busy    : OUT STD_LOGIC);
 END MasterJoystick;
 
 ARCHITECTURE behavior OF MasterJoystick IS
 	TYPE t_state IS (idle, sync, exchange, exchange_wait);
 	SIGNAL state : t_state;
+
+	COMPONENT diviseurClk IS
+		-- facteur : ratio entre la fréquence de l'horloge origine et celle
+		--           de l'horloge générée
+		--  ex : 100 MHz -> 1Hz : 100 000 000
+		--  ex : 100 MHz -> 1kHz : 100 000
+		GENERIC (facteur : NATURAL);
+		PORT (
+			clk, reset : IN STD_LOGIC;
+			nclk       : OUT STD_LOGIC);
+	END COMPONENT;
 
 	COMPONENT er_1octet
 		PORT (
@@ -35,26 +48,39 @@ ARCHITECTURE behavior OF MasterJoystick IS
 			busy : OUT STD_LOGIC);
 	END COMPONENT;
 
+	SIGNAL nclk : STD_LOGIC; -- divided clock
+
+	SIGNAL reg_inLed     : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL reg_x_joy_low : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	SIGNAL reg_y_joy_low : STD_LOGIC_VECTOR(7 DOWNTO 0);
+
 	SIGNAL din    : STD_LOGIC_VECTOR(7 DOWNTO 0);
 	SIGNAL dout   : STD_LOGIC_VECTOR(7 DOWNTO 0);
 	SIGNAL enER   : STD_LOGIC;
-	SIGNAL enBusy : STD_LOGIC;
+	SIGNAL busyER : STD_LOGIC;
 BEGIN
+
+	diviseurClk_inst : diviseurClk GENERIC MAP(facteur => 100)
+	PORT MAP(
+		clk   => clk,
+		reset => rst,
+		nclk  => nclk
+	);
 
 	er_1octet_inst : er_1octet
 	PORT MAP(
 		rst  => rst,
-		clk  => clk,
+		clk  => nclk,
 		en   => enER,
 		din  => din,
 		miso => miso,
 		sclk => sclk,
 		mosi => mosi,
 		dout => dout,
-		busy => enBusy
+		busy => busyER
 	);
 
-	PROCESS (clk, rst)
+	PROCESS (nclk, rst)
 		VARIABLE tick_count     : NATURAL := 0;
 		VARIABLE exchange_count : NATURAL := 0;
 		VARIABLE isLast         : BOOLEAN := false;
@@ -69,7 +95,7 @@ BEGIN
 			busy  <= '0';
 			ss    <= '1';
 			state <= idle;
-		ELSIF rising_edge(clk) THEN
+		ELSIF rising_edge(nclk) THEN
 			CASE state IS
 				WHEN idle =>
 					IF (en = '1') THEN
@@ -77,6 +103,9 @@ BEGIN
 						busy  <= '1';
 						ss    <= '0';
 						state <= sync;
+
+						-- We store the input data in a register
+						reg_inLed <= "100000" & inLed1 & inLed2;
 					END IF;
 
 				WHEN sync =>
@@ -89,42 +118,92 @@ BEGIN
 				WHEN exchange =>
 					CASE exchange_count IS
 						WHEN 0 =>
-							IF sending = false AND enER = '0' AND enBusy = '0' THEN
-								din  <= v1;
+							IF (NOT sending) AND enER = '0' AND busyER = '0' THEN
+								-- The first message is the inLed, so we send the inLed register
+								din  <= reg_inLed;
 								enER <= '1';
 								sending := true;
-							ELSIF sending = true AND enER = '1' AND enBusy = '0' THEN
+							ELSIF sending AND enER = '1' AND busyER = '0' THEN
 								enER <= '0';
-							ELSIF sending = true AND enER = '0' AND enBusy = '0' THEN
+							ELSIF sending AND enER = '0' AND busyER = '0' THEN
 								sending := false;
-								val_xor <= dout;
-								state   <= exchange_wait;
+
+								-- The first message is the x_joy_low, so we store it
+								-- in a register in order to concatenate it with the
+								-- x_joy_high.
+								reg_x_joy_low <= dout;
+								state         <= exchange_wait;
 							END IF;
 
 						WHEN 1 =>
-							IF sending = false AND enER = '0' AND enBusy = '0' THEN
-								din  <= v2;
-								enER <= '1';
-								sending := true;
-							ELSIF sending = true AND enER = '1' AND enBusy = '0' THEN
-								enER <= '0';
-							ELSIF sending = true AND enER = '0' AND enBusy = '0' THEN
-								sending := false;
-
-								val_and <= dout;
-								state   <= exchange_wait;
-							END IF;
-
-						WHEN 2 =>
-							IF sending = false AND enER = '0' AND enBusy = '0' THEN
+							-- The second message is the x_joy_high, at the end
+							-- we concatenate it with the x_joy_low with the output
+							-- we do nothing in input
+							IF (NOT sending) AND enER = '0' AND busyER = '0' THEN
 								din  <= "00000000";
 								enER <= '1';
 								sending := true;
-							ELSIF sending = true AND enER = '1' AND enBusy = '0' THEN
+							ELSIF sending AND enER = '1' AND busyER = '0' THEN
 								enER <= '0';
-							ELSIF sending = true AND enER = '0' AND enBusy = '0' THEN
+							ELSIF sending AND enER = '0' AND busyER = '0' THEN
 								sending := false;
-								val_or <= dout;
+
+								x_joy <= dout(1 DOWNTO 0) & reg_x_joy_low;
+								state <= exchange_wait;
+							END IF;
+
+						WHEN 2 =>
+							-- The third message is the y_joy_low, we store
+							-- it in a register in order to concatenate it with
+							-- the y_joy_high.
+							-- We do nothing in input
+							IF (NOT sending) AND enER = '0' AND busyER = '0' THEN
+								din  <= "00000000";
+								enER <= '1';
+								sending := true;
+							ELSIF sending AND enER = '1' AND busyER = '0' THEN
+								enER <= '0';
+							ELSIF sending AND enER = '0' AND busyER = '0' THEN
+								sending := false;
+
+								reg_y_joy_low <= dout;
+								state         <= exchange_wait;
+							END IF;
+
+						WHEN 3 =>
+							-- The fourth message is the y_joy_high, at the end
+							-- we concatenate it with the y_joy_low with the output
+							-- we do nothing in input
+							IF (NOT sending) AND enER = '0' AND busyER = '0' THEN
+								din  <= "00000000";
+								enER <= '1';
+								sending := true;
+							ELSIF sending AND enER = '1' AND busyER = '0' THEN
+								enER <= '0';
+							ELSIF sending AND enER = '0' AND busyER = '0' THEN
+								sending := false;
+
+								y_joy <= dout(1 DOWNTO 0) & reg_y_joy_low;
+								state <= exchange_wait;
+							END IF;
+
+						WHEN 4 =>
+							-- The fifth message is the state of the buttons,
+							-- we split it in the 3 buttons. and set the output
+							-- signals that correspond to the buttons.
+							-- 							
+							IF (NOT sending) AND enER = '0' AND busyER = '0' THEN
+								din  <= "00000000";
+								enER <= '1';
+								sending := true;
+							ELSIF sending AND enER = '1' AND busyER = '0' THEN
+								enER <= '0';
+							ELSIF sending AND enER = '0' AND busyER = '0' THEN
+								sending := false;
+
+								btn_2   <= dout(2);
+								btn_1   <= dout(1);
+								btn_joy <= dout(0);
 
 								isLast := true;
 								state <= exchange_wait;
--- src/joystick/Nexys4Joystick.vhd
+++ src/joystick/Nexys4Joystick.vhd
@@ -1,42 +1,111 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.std_logic_arith.all;
-use IEEE.std_logic_unsigned.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.std_logic_arith.ALL;
+USE IEEE.std_logic_unsigned.ALL;
 
-entity Nexys4Joystick is
-  port (
+ENTITY Nexys4Joystick IS
+  PORT (
     -- les 16 switchs
-    swt : in std_logic_vector (15 downto 0);
+    swt : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
     -- les 5 boutons noirs
-    btnC, btnU, btnL, btnR, btnD : in std_logic;
+    btnC, btnU, btnL, btnR, btnD : IN STD_LOGIC;
     -- horloge
-    mclk : in std_logic;
+    mclk : IN STD_LOGIC;
+
     -- les 16 leds
-    led : out std_logic_vector (15 downto 0);
+    led : OUT STD_LOGIC_VECTOR (15 DOWNTO 0);
     -- les anodes pour sélectionner les afficheurs 7 segments à utiliser
-    an : out std_logic_vector (7 downto 0);
+    an : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
     -- valeur affichée sur les 7 segments (point décimal compris, segment 7)
-    ssg : out std_logic_vector (7 downto 0)
+    ssg : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+
+    -- les 3 signaux de communication SPI 
+    ss   : OUT STD_LOGIC;
+    mosi : OUT STD_LOGIC;
+    miso : IN STD_LOGIC;
+    sclk : OUT STD_LOGIC
   );
-end Nexys4Joystick;
+END Nexys4Joystick;
 
-architecture synthesis of Nexys4Joystick is
+ARCHITECTURE synthesis OF Nexys4Joystick IS
 
-  -- rappel du (des) composant(s)
-  -- À COMPLÉTER 
+  COMPONENT MasterJoystick IS
+    PORT (
+      rst     : IN STD_LOGIC;
+      clk     : IN STD_LOGIC;
+      en      : IN STD_LOGIC;
+      inLed1  : IN STD_LOGIC;
+      inLed2  : IN STD_LOGIC;
+      miso    : IN STD_LOGIC;
+      ss      : OUT STD_LOGIC;
+      sclk    : OUT STD_LOGIC;
+      mosi    : OUT STD_LOGIC;
+      x_joy   : OUT STD_LOGIC_VECTOR (9 DOWNTO 0);
+      y_joy   : OUT STD_LOGIC_VECTOR (9 DOWNTO 0);
+      btn_joy : OUT STD_LOGIC;
+      btn_1   : OUT STD_LOGIC;
+      btn_2   : OUT STD_LOGIC
+    );
+  END COMPONENT;
 
-begin
+  COMPONENT All7Segments IS
+    PORT (
+      clk   : IN STD_LOGIC;
+      reset : IN STD_LOGIC;
+      e0    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+      e1    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+      e2    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+      e3    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+      e4    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+      e5    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+      e6    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+      e7    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+      an    : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+      ssg   : OUT STD_LOGIC_VECTOR (7 DOWNTO 0));
+  END COMPONENT;
 
-  -- valeurs des sorties (à modifier)
+  SIGNAL reset : STD_LOGIC;
+  SIGNAL x_joy : STD_LOGIC_VECTOR(9 DOWNTO 0);
+  SIGNAL y_joy : STD_LOGIC_VECTOR(9 DOWNTO 0);
+BEGIN
+  reset <= NOT btnC;
 
-  -- convention afficheur 7 segments 0 => allumé, 1 => éteint
-  ssg <= (others => '1');
-  -- aucun afficheur sélectionné
-  an(7 downto 0) <= (others => '1');
   -- 16 leds éteintes
-  led(15 downto 0) <= (others => '0');
+  led(15 DOWNTO 3) <= (OTHERS => '0');
 
-  -- connexion du (des) composant(s) avec les ports de la carte
-  -- À COMPLÉTER 
-    
-end synthesis;
+  -- instanciation du composant Master
+  MasterJoystick_inst : MasterJoystick
+  PORT MAP(
+    rst     => reset,
+    clk     => mclk,
+    en      => '1',
+    inLed1  => btnL,
+    inLed2  => btnR,
+    miso    => miso,
+    ss      => ss,
+    sclk    => sclk,
+    mosi    => mosi,
+    x_joy   => x_joy,
+    y_joy   => y_joy,
+    btn_joy => led(2),
+    btn_1   => led(0),
+    btn_2   => led(1)
+  );
+
+  All7Segments_inst : All7Segments
+  PORT MAP(
+    clk   => mclk,
+    reset => reset,
+    e7    => "0000",
+    e6    => "00" & x_joy(9 DOWNTO 8),
+    e5    => x_joy(7 DOWNTO 4),
+    e4    => x_joy(3 DOWNTO 0),
+
+    e3  => "0000",
+    e2  => "00" & y_joy(9 DOWNTO 8),
+    e1  => y_joy(7 DOWNTO 4),
+    e0  => y_joy(3 DOWNTO 0),
+    an  => an,
+    ssg => ssg
+  );
+END synthesis;
